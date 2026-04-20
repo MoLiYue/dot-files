fd624e2dae2042ff127f2ae17ecb822cf0bce700
MoLiYue <moliyue018@gmail.com>
2026-04-20 21:47:55 +0800

    add tmux config

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ ln -s $PWD/waybar ~/.config/waybar
 ln -s $PWD/hypr ~/.config/hypr
 ln -s $PWD/starship.toml ~/.config/starship.toml
 ln -s $PWD/fastfetch ~/.config/fastfetch
+ln -s $PWD/tmux/tmux.conf ~/.config/tmux/tmux.conf
 
 # install fonts
 # yay -S
