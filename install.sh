847cd08c8811057880e8c43c81ef8276c773fe09
MoLiYue <moliyue018@gmail.com>
2026-04-20 20:24:17 +0800

    add starship config

--- install.sh
+++ install.sh
@@ -2,9 +2,10 @@
 
 ln -s ./waybar ~/.config/waybar
 ln -s ./hypr ~/.config/hypr
+ln -s ./starship.toml ~/.config/starship.toml
 
 # install fonts
-# yay -S 
+# yay -S
 #
 # install apps
 # yay -S kitty wofi htop s-tui pacmixer wlogout
