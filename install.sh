e6da089bc3fdedfce33e71e8a8e2b6142be3bdbb
MoLiYue <moliyue018@gmail.com>
2026-04-21 14:47:33 +0800

    add wofi config

--- install.sh
+++ install.sh
@@ -7,6 +7,7 @@ ln -s $PWD/fastfetch ~/.config/fastfetch
 ln -s $PWD/tmux/tmux.conf ~/.config/tmux/tmux.conf
 ln -s $PWD/yazi ~/.config/yazi
 ln -s $PWD/zshrc/.zshrc ~/.zshrc
+ln -s $PWD/wofi ~/.config/wofi
 
 # install fonts
 # yay -S
